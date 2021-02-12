16239bfa6377e4731b7f5d3fc7ae81f0279c2dd0
Aaron Utterback <Autterback3@mail.greenriver.edu>
2021-02-11 19:47:32 -0500

    Updated controller, added views for create 2 and create 3, created data-layer for interests array outdoor/indoor

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ error_reporting(E_ALL);
 
 // require the autoload file
 require_once('vendor/autoload.php');
+require_once('model/data-layer.php');
 
 // create an instance of the base class
 $f3 = Base::instance();
@@ -21,6 +22,23 @@ $f3->route('GET /create1' , function(){
     // fat free - taking the view page and rendering it in the browser
     $view = new Template();
     echo $view->render('views/create1.html');
+});
+
+// Dating -- Create Account -- Profile
+$f3->route('POST /create2' , function(){
+    // fat free - taking the view page and rendering it in the browser
+    $view = new Template();
+    echo $view->render('views/create2.html');
+});
+
+// Dating -- Create Account -- Interests
+$f3->route('POST /create3' , function($f3){
+    // fat free - taking the view page and rendering it in the browser
+    // HIVE
+    $f3->set('indoor', getIndoorInterests());
+    $f3->set('outdoor', getOutdoorInterests());
+    $view = new Template();
+    echo $view->render('views/create3.html');
 });
 
 // Run fat free
---
+++ model/data-layer.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Function to hold array of indoor interests
+ */
+function getIndoorInterests(){
+    return array("tv", "movies", "cooking", "board games", "puzzles", "reading", "playing cards", "video games");
+}
+
+/*
+ * Function to hold array of outdoor interests
+ */
+function getOutdoorInterests(){
+    return array("hiking", "biking", "swimming", "collecting", "walking", "climbing", "camping", "athletics");
+}
